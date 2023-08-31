7012d3e0c272e8930542c927d3680df45f9b7fe0
gabrielsopimentel <gabrielsopimentel@gmail.com>
2023-08-31 12:06:55 -0300

    model conta

--- contabancaria/Program.cs
+++ contabancaria/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace contabancaria
+﻿using contabancaria.Model;
+
+namespace contabancaria
 {
     internal class Program
     {
@@ -6,6 +8,20 @@
         static void Main(string[] args)
         {
             int opcao;
+
+            Conta c1 = new Conta(1, 123, 1, "Gaspar", 1000000.00M);
+
+            c1.Visualizar();
+            c1.SetNumero(345);
+            c1.Visualizar();
+
+            c1.Sacar(1000);
+
+            c1.Visualizar();
+
+            c1.Depositar(5000);
+
+            c1.Visualizar();
 
             while (true)
             {
