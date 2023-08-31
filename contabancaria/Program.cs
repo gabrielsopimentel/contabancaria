e56cc909a7451ac24bb0f6ce22443b663350f92c
gabrielsopimentel <gabrielsopimentel@gmail.com>
2023-08-30 22:36:14 -0300

    key press

--- contabancaria/Program.cs
+++ contabancaria/Program.cs
@@ -2,6 +2,7 @@
 {
     internal class Program
     {
+        private static ConsoleKeyInfo consoleKeyInfo;
         static void Main(string[] args)
         {
             int opcao;
@@ -9,7 +10,7 @@
             while (true)
             {
                 Console.BackgroundColor = ConsoleColor.Black;
-                Console.ForegroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Blue;
 
                 Console.WriteLine("*****************************************************");
                 Console.WriteLine("                                                     ");
@@ -36,46 +37,80 @@
                 switch (opcao)
                 {
                     case 1:
+                        Console.ForegroundColor = ConsoleColor.Green;
                         Console.WriteLine("Criar Conta\n\n");
+                        Console.ResetColor();
 
+                        KeyPress();
                         break;
                     case 2:
+                        Console.ForegroundColor = ConsoleColor.Green;
                         Console.WriteLine("Listar todas as Contas\n\n");
+                        Console.ResetColor();
 
+                        KeyPress();
                         break;
                     case 3:
+                        Console.ForegroundColor = ConsoleColor.Green;
                         Console.WriteLine("Consultar dados da Conta - por número\n\n");
+                        Console.ResetColor();
 
+                        KeyPress();
                         break;
                     case 4:
+                        Console.ForegroundColor = ConsoleColor.Green;
                         Console.WriteLine("Atualizar dados da Conta\n\n");
+                        Console.ResetColor();
 
+                        KeyPress();
                         break;
                     case 5:
+                        Console.ForegroundColor = ConsoleColor.Green;
                         Console.WriteLine("Apagar a Conta\n\n");
+                        Console.ResetColor();
 
+                        KeyPress();
                         break;
                     case 6:
+                        Console.ForegroundColor = ConsoleColor.Green;
                         Console.WriteLine("Saque\n\n");
+                        Console.ResetColor();
 
+                        KeyPress();
                         break;
                     case 7:
+                        Console.ForegroundColor = ConsoleColor.Green;
                         Console.WriteLine("Depósito\n\n");
+                        Console.ResetColor();
 
+                        KeyPress();
                         break;
                     case 8:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Transferência entre Contas\n\n");
+                        Console.ResetColor();
+
+                        KeyPress();
+                        break;
                         Console.WriteLine("Transferência entre Contas\n\n");
                         break;
                     case 9:
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.Green;
                         Console.WriteLine("\nBanco do Brazil com Z - O seu Futuro começa aqui!");
                         Sobre();
+                        Console.ResetColor();
                         System.Environment.Exit(0);
 
                         break;
                     default:
+                        Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("\nOpção Inválida!\n");
+                        Console.ResetColor();
 
+                        KeyPress();
                         break;
+
                 }
             }
 
@@ -86,6 +121,15 @@
                 Console.WriteLine("Generation Brasil - gabrielsopimentel@gmail.com");
                 Console.WriteLine("github.com/gabrielsopimentel");
                 Console.WriteLine("*********************************************************");
+            }
+
+            static void KeyPress()
+            {
+                do
+                {
+                    Console.Write("\nPressione Enter para Continuar...");
+                    consoleKeyInfo = Console.ReadKey();
+                } while (consoleKeyInfo.Key != ConsoleKey.Enter);
             }
         }
     }
